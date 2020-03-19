b3d6e74ba930133a0fea51c84e7ca9119533eec5
madonghao <madonghao>
2020-03-19 13:40:31 +0800

    fix: payment项目的初始化

---
+++ cloud-provider-payment8001/src/main/java/com/mdh/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.mdh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 支付模块的启动类
+ *
+ * @author madonghao
+ * @create 2020-03-19 13:34
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
