5cdb6b8bd2d6095d646fef3fe0fa6cecb5772822
madonghao <madonghao>
2020-03-24 22:12:58 +0800

    fix: Eureka单机版

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # springCloud2020
+
+## eureka
+~~~
+最新的
+<dependency>
+   <groupId>org.springframework.cloud</groupId>
+   <artifactId>spring-cloud-starter-netflix-eureka-server</artifactId>
+</dependency>
+
+
+旧版
+<dependency>
+    <groupId>org.springframework.cloud</groupId>
+    <artifactId>spring-cloud-starter-eureka</artifactId>
+    <version>1.0.5.RELEASE</version>
+</dependency>
+~~~
---
+++ cloud-eureka-server7001/src/main/java/com/mdh/springcloud/EurekaMain7001.java
@@ -0,0 +1,20 @@
+package com.mdh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka服务端启动类
+ * Eureka注册中心7001端口
+ *
+ * @author madonghao
+ * @create 2020-03-23 15:43
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
