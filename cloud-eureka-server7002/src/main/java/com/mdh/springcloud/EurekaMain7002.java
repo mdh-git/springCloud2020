bcc93c305d78cfdaf4b16ba3420c71c6ed7922bc
madonghao <madonghao>
2020-03-25 00:44:02 +0800

    fix: eureka7001 和 eureka7002 集群

---
+++ cloud-eureka-server7002/src/main/java/com/mdh/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.mdh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka服务端7002
+ *
+ * @author madonghao
+ * @create 2020-03-24 23:37
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
