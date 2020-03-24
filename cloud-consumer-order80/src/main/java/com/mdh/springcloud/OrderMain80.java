77ab0bffc79ebc1869de45f52aa98d3fcebdc4b8
madonghao <madonghao>
2020-03-24 22:25:22 +0800

    fix: order80注册到Eureka

--- cloud-consumer-order80/src/main/java/com/mdh/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/mdh/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.mdh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author madonghao
  * @create 2020-03-21 18:36
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
