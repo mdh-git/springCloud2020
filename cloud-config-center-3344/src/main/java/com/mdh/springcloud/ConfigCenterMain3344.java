1796d9a5edeb77171ebfc65a8806adfb67a73745
madonghao <madonghao>
2020-04-23 17:55:35 +0800

    fix: 实现springCloud-config通过gitHub获取配置信息

---
+++ cloud-config-center-3344/src/main/java/com/mdh/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.mdh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author madonghao
+ * @create 2020-04-20 15:53
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
